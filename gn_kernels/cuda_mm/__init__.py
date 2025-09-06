fix ldmatrix address computation for 8-bit

gn_kernels/cuda_mm/__init__.py
@@ -48,7 +48,7 @@ def cdiv(a, b):
 class MatmulKernel:
     in_dtype: torch.dtype = torch.bfloat16
     out_dtype: torch.dtype = torch.bfloat16
-    acc_dtype: torch.dtype = torch.float
+    acc_dtype: torch.dtype = torch.float32
     block_mnk: tuple[int, int, int] = (128, 128, 64)
     warp_mn: tuple[int, int] = (2, 2)
     num_stages: int = 1
@@ -96,7 +96,7 @@ class MatmulKernel:
 
         M, K = A.shape
         _, N = B.shape
-        C = A.new_zeros(M, N, dtype=self.out_dtype)
+        C = A.new_empty(M, N, dtype=self.out_dtype)
 
         grid = cdiv(M, self.block_mnk[0]) * cdiv(N, self.block_mnk[1])
         self.kernel((grid, 1, 1), self.tb_size, (A, B, C, M, N, K), self.smem_size)

tests/test_cuda_mm.py
@@ -3,39 +3,50 @@ import torch
 
 from gn_kernels.cuda_mm import MatmulKernel
 
-_DTYPE_LOOKUP = dict(
-    fp32=torch.float32,
-    fp16=torch.float16,
-    bf16=torch.bfloat16,
-    fp8_e4m3=torch.float8_e4m3fn,
-    fp8_e5m2=torch.float8_e5m2,
-    int32=torch.int32,
-    int8=torch.int8,
-    uint8=torch.uint8,
-)
-
 
 # TODO:
-# - FP8 and INT8
 # - FP16 accumulation for FP16 and FP8
-@pytest.mark.parametrize(
-    "in_dtype,out_dtype,acc_dtype",
-    [
-        ("bf16", "bf16", "fp32"),
-        ("fp16", "fp16", "fp32"),
-    ],
-)
-def test_cuda_mm(in_dtype: str, out_dtype: str, acc_dtype: str):
-    in_dtype = _DTYPE_LOOKUP[in_dtype]
-    out_dtype = _DTYPE_LOOKUP[out_dtype]
-    acc_dtype = _DTYPE_LOOKUP[acc_dtype]
-
-    kernel = MatmulKernel(in_dtype, out_dtype, acc_dtype, num_stages=2)
+@pytest.mark.parametrize("dtype", ["fp16", "bf16"])
+def test_cuda_mm_fp(dtype: str):
+    dtype = dict(fp16=torch.float16, bf16=torch.bfloat16)[dtype]
+    kernel = MatmulKernel(dtype, dtype, torch.float32, num_stages=2)
 
     M, N, K = 512, 768, 1024
-    A = torch.randn(M, K, dtype=in_dtype, device="cuda")
-    B = torch.randn(N, K, dtype=in_dtype, device="cuda").T
+    A = torch.randn(M, K, dtype=dtype, device="cuda")
+    B = torch.randn(N, K, dtype=dtype, device="cuda").T
 
     actual = kernel.run(A, B)
+    torch.cuda.synchronize()
+
     expected = torch.mm(A, B)
+    torch.testing.assert_close(actual, expected)
+
+
+def test_cuda_mm_int8():
+    kernel = MatmulKernel(torch.int8, torch.int32, torch.int32, num_stages=2)
+
+    M, N, K = 512, 768, 1024
+    A = torch.randint(-128, 127, (M, K), dtype=torch.int8, device="cuda")
+    B = torch.randint(-128, 127, (N, K), dtype=torch.int8, device="cuda").T
+
+    actual = kernel.run(A, B)
+    torch.cuda.synchronize()
+
+    expected = torch._int_mm(A, B)
+    torch.testing.assert_close(actual, expected)
+
+
+def test_cuda_mm_fp8():
+    dtype = torch.float8_e4m3fn
+    kernel = MatmulKernel(dtype, torch.bfloat16, torch.float32, num_stages=2)
+
+    M, N, K = 512, 768, 1024
+    A = torch.randn(M, K, device="cuda").to(dtype)
+    B = torch.randn(N, K, device="cuda").to(dtype).T
+
+    actual = kernel.run(A, B)
+    torch.cuda.synchronize()
+
+    scale = torch.ones(1, device="cuda")
+    expected = torch._scaled_mm(A, B, scale, scale, out_dtype=torch.bfloat16)
     torch.testing.assert_close(actual, expected)