433e47b921aa322d4313278b0a329bea19729d2c
Thien Tran <gau.nernst@yahoo.com.sg>
2025-09-13 20:32:02 +0800

    triton_attn: handle causal + q_len > kv_len. add varlen

--- gn_kernels/triton_attn.py
+++ gn_kernels/triton_attn.py
@@ -75,6 +75,7 @@ def _forward_loop(
     return acc, m_i, l_i
 
 
+# TODO: change this to unify with varlen kernel
 @triton.heuristics(dict(BOUNDS_CHECK=lambda args: args["KV_LEN"] % args["BLOCK_N"] > 0))
 @triton.jit
 def triton_attn_kernel(
@@ -113,16 +114,17 @@ def triton_attn_kernel(
 
     # select the input/output
     Q_ptr += bs_id * Q_stride[0] + head_id_q * Q_stride[2]
+    O_ptr += bs_id * O_stride[0] + head_id_q * O_stride[2]
     K_ptr += bs_id * K_stride[0] + head_id_kv * K_stride[2]
     V_ptr += bs_id * V_stride[0] + head_id_kv * V_stride[2]
-    O_ptr += bs_id * O_stride[0] + head_id_q * O_stride[2]
     if SCALED_QK:
         scale_Q_ptr += (bs_id * Q_HEADS + head_id_q) * Q_LEN
         scale_K_ptr += (bs_id * KV_HEADS + head_id_kv) * KV_LEN
 
     # initialize attention state
+    # don't use -inf for initial value of max to avoid (-inf) - (-inf) = nan
     acc = tl.zeros([BLOCK_M, V_DIM], dtype=tl.float32)
-    m_i = tl.zeros([BLOCK_M], dtype=tl.float32) - float("inf")  # max
+    m_i = tl.zeros([BLOCK_M], dtype=tl.float32) - 1e38  # max
     l_i = tl.zeros([BLOCK_M], dtype=tl.float32) + 1.0  # sumexp
 
     # e^x = e^(log(2) * x / log(2)) = 2^(x/log(2))
@@ -161,15 +163,15 @@ def triton_attn_kernel(
     else:
         # for causal, we align the last q with the last kv i.e. causal lower right. this is FA's
         # default behavior to make decode easier. in other words:
-        # - q[0]       will attend to kv[0 : LEN_KV-LEN_Q]
-        # - q[LEN_Q-1] will attend to kv[0 : LEN_KV] (full)
+        # - q[0]       will attend to kv[0 : KV_LEN-Q_LEN+1]
+        # - q[Q_LEN-1] will attend to kv[0 : KV_LEN] (full)
         #
         # we will split KV loop into 2:
         # 1. full KV blocks: don't need to apply causal mask within a block
         # 2. partial KV block: need to apply causal mask within a block
         #
         # this threadblock is responsible for q[pid_m * BLOCK_M : (pid_m + 1) * BLOCK_M]
-        # we have full KV blocks for kv[0 : LEN_KV-LEN_Q + pid_m * BLOCK_M]
+        # we have full KV blocks for kv[0 : KV_LEN-Q_LEN + pid_m * BLOCK_M]
         # and partial KV blocks for the remaining.
         # we also enable bounds check for the partial blocks loop, since the partial blocks are
         # guaranteed to contain the last block.
@@ -226,6 +228,13 @@ def triton_attn_kernel(
     # epilogue
     acc = acc / l_i[:, None]
     offs_m = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)[:, None]
+
+    # mask out the elements that don't attend to anything.
+    # i think if the code above is implemented correctly, i don't
+    # actually need this. but for some reasons, i do...
+    if CAUSAL:
+        acc = tl.where(KV_LEN - Q_LEN + offs_m >= 0, acc, 0.0)
+
     offs_dim_v = tl.arange(0, V_DIM)[None, :]
     o_ptrs = O_ptr + offs_m * O_stride[1] + offs_dim_v
     tl.store(o_ptrs, acc, mask=offs_m < Q_LEN)
@@ -311,5 +320,197 @@ def triton_attn(
 
     else:
         triton_attn_kernel_autotune[grid](**kwargs)
+
+    return o
+
+
+@triton.jit
+def triton_varlen_attn_kernel(
+    Q_ptr,  # [Q_LEN_TOTAL, Q_HEADS, QK_DIM]
+    K_ptr,  # [KV_LEN_TOTAL, KV_HEADS, QK_DIM]
+    V_ptr,  # [KV_LEN_TOTAL, KV_HEADS, V_DIM]
+    O_ptr,  # [Q_LEN_TOTAL, Q_HEADS, V_DIM]
+    Q_stride,
+    K_stride,
+    V_stride,
+    O_stride,
+    Q_offsets_ptr,  # [BS+1]
+    KV_offsets_ptr,  # [BS+1]
+    # problem shape
+    Q_HEADS: int,
+    KV_HEADS: int,
+    QK_DIM: tl.constexpr,
+    V_DIM: tl.constexpr,
+    # kernel params
+    BLOCK_M: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+    # others
+    sm_scale: float | None = None,
+    CAUSAL: tl.constexpr = False,
+    QK_ACC_DTYPE: tl.constexpr = tl.float32,
+    PV_ACC_DTYPE: tl.constexpr = tl.float32,
+):
+    SCALED_QK: tl.constexpr = False
+    BOUNDS_CHECK: tl.constexpr = True
+
+    head_id_q = tl.program_id(0)  # L2 reuse of KV across heads
+    head_id_kv = head_id_q // (Q_HEADS // KV_HEADS)
+    pid_m = tl.program_id(1)
+    bs_id = tl.program_id(2)
+
+    Q_start = tl.load(Q_offsets_ptr + bs_id)
+    Q_end = tl.load(Q_offsets_ptr + bs_id + 1)
+    Q_LEN = Q_end - Q_start
+
+    KV_start = tl.load(KV_offsets_ptr + bs_id)
+    KV_end = tl.load(KV_offsets_ptr + bs_id + 1)
+    KV_LEN = KV_end - KV_start
+
+    # early exit
+    if pid_m * BLOCK_M > Q_end:
+        return
+
+    # select the input/output
+    Q_ptr += Q_start * Q_stride[0] + head_id_q * Q_stride[1]
+    O_ptr += Q_start * O_stride[0] + head_id_q * O_stride[1]
+    K_ptr += KV_start * K_stride[0] + head_id_kv * K_stride[1]
+    V_ptr += KV_start * V_stride[0] + head_id_kv * V_stride[1]
+
+    # initialize attention state
+    acc = tl.zeros([BLOCK_M, V_DIM], dtype=tl.float32)
+    m_i = tl.zeros([BLOCK_M], dtype=tl.float32) - float("inf")  # max
+    l_i = tl.zeros([BLOCK_M], dtype=tl.float32) + 1.0  # sumexp
+
+    # e^x = e^(log(2) * x / log(2)) = 2^(x/log(2))
+    if sm_scale is None:
+        sm_scale = QK_DIM**-0.5
+    sm_scale *= 1.44269504  # 1/log(2)
+
+    offs_m = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
+    offs_n = tl.arange(0, BLOCK_N)
+    offs_dim_qk = tl.arange(0, QK_DIM)
+    offs_dim_v = tl.arange(0, V_DIM)
+
+    q_ptrs = Q_ptr + offs_m[:, None] * Q_stride[0] + offs_dim_qk[None, :]  # [BLOCK_M, DIM_QK]
+    k_ptrs = K_ptr + offs_n[None, :] * K_stride[0] + offs_dim_qk[:, None]  # [QK_DIM, BLOCK_N]
+    v_ptrs = V_ptr + offs_n[:, None] * V_stride[0] + offs_dim_v[None, :]  # [BLOCK_N, V_DIM]
+
+    # load q. it will stay in rmem throughout
+    q = tl.load(q_ptrs, mask=offs_m[:, None] < Q_LEN)
+
+    if not CAUSAL:
+        end = KV_LEN
+    else:
+        end = (KV_LEN - Q_LEN + pid_m * BLOCK_M) // BLOCK_N * BLOCK_N
+
+    # full blocks
+    FULL_ARGS: tl.constexpr = (BLOCK_N, False, SCALED_QK, QK_ACC_DTYPE, PV_ACC_DTYPE, False if CAUSAL else BOUNDS_CHECK)
+    acc, m_i, l_i = _forward_loop(
+        (acc, m_i, l_i),
+        (0, end, KV_LEN),
+        q,
+        k_ptrs,
+        v_ptrs,
+        None,  # scale_k_ptrs
+        K_stride[0],
+        V_stride[0],
+        None,  # offs_m
+        sm_scale,
+        FULL_ARGS,
+    )
+
+    if CAUSAL:
+        # handle partial blocks in causal attention
+        start = end
+        end = KV_LEN - Q_LEN + (pid_m + 1) * BLOCK_M
+
+        k_ptrs += start * K_stride[0]
+        v_ptrs += start * V_stride[0]
+
+        # this is only used for causal mask within a block
+        # to use causal upper left, remove this line
+        offs_m += KV_LEN - Q_LEN
+
+        PARTIAL_ARGS: tl.constexpr = (BLOCK_N, CAUSAL, SCALED_QK, QK_ACC_DTYPE, PV_ACC_DTYPE, BOUNDS_CHECK)
+        acc, m_i, l_i = _forward_loop(
+            (acc, m_i, l_i),
+            (start, end, KV_LEN),
+            q,
+            k_ptrs,
+            v_ptrs,
+            None,  # scale_k_ptrs
+            K_stride[0],
+            V_stride[0],
+            offs_m,
+            sm_scale,
+            PARTIAL_ARGS,
+        )
+
+    # epilogue
+    acc = acc / l_i[:, None]
+    offs_m = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)[:, None]
+
+    if CAUSAL:
+        acc = tl.where(KV_LEN - Q_LEN + offs_m >= 0, acc, 0.0)
+
+    offs_dim_v = tl.arange(0, V_DIM)[None, :]
+    o_ptrs = O_ptr + offs_m * O_stride[0] + offs_dim_v
+    tl.store(o_ptrs, acc, mask=offs_m < Q_LEN)
+
+
+def triton_varlen_attn(
+    q: Tensor,
+    k: Tensor,
+    v: Tensor,
+    q_offsets: Tensor,
+    kv_offsets: Tensor,
+    max_q_len: int,
+    *,
+    causal: bool = False,
+    qk_fp16_acc: bool = False,
+    pv_fp16_acc: bool = False,
+):
+    Q_LEN_TOTAL, Q_HEADS, QK_DIM = q.shape
+    KV_LEN_TOTAL, KV_HEADS, V_DIM = v.shape
+    BS = q_offsets.shape[0] - 1
+    assert Q_HEADS % KV_HEADS == 0
+    assert k.shape == (KV_LEN_TOTAL, KV_HEADS, QK_DIM)
+    assert kv_offsets.shape[0] == BS + 1
+    assert q.stride(-1) == 1
+    assert k.stride(-1) == 1
+    assert v.stride(-1) == 1
+    assert q_offsets.is_contiguous()
+    assert kv_offsets.is_contiguous()
+
+    def grid(args):
+        return (Q_HEADS, triton.cdiv(max_q_len, args["BLOCK_M"]), BS)
+
+    o = q.new_empty(Q_LEN_TOTAL, Q_HEADS, V_DIM, dtype=v.dtype)
+
+    kwargs = dict(
+        Q_ptr=q,
+        K_ptr=k,
+        V_ptr=v,
+        O_ptr=o,
+        Q_stride=q.stride(),
+        K_stride=k.stride(),
+        V_stride=v.stride(),
+        O_stride=o.stride(),
+        Q_offsets_ptr=q_offsets,
+        KV_offsets_ptr=kv_offsets,
+        Q_HEADS=Q_HEADS,
+        KV_HEADS=KV_HEADS,
+        QK_DIM=QK_DIM,
+        V_DIM=V_DIM,
+        BLOCK_M=64,
+        BLOCK_N=64,
+        CAUSAL=causal,
+        QK_ACC_DTYPE=tl.float16 if qk_fp16_acc else tl.float32 if q.is_floating_point() else tl.int32,
+        PV_ACC_DTYPE=tl.float16 if pv_fp16_acc else tl.float32,
+        num_warps=4,
+        num_stages=3,
+    )
+
+    triton_varlen_attn_kernel[grid](**kwargs)
 
     return o
--- tests/test_triton_attn.py
+++ tests/test_triton_attn.py
@@ -1,25 +1,17 @@
 import pytest
 import torch
 import torch.nn.functional as F
+from torch import Tensor
 from torch.nn.attention.bias import causal_lower_right
 
-from gn_kernels.triton_attn import triton_attn
+from gn_kernels.triton_attn import triton_attn, triton_varlen_attn
 
 
-@pytest.mark.parametrize("q_len,kv_len", [(64, 256), (33, 123)])
-@pytest.mark.parametrize("causal", [False, True])
-def test_triton_attn(q_len: int, kv_len: int, causal: bool):
-    bs = 2
-    num_heads_q = 8
-    num_heads_kv = 4
-    head_dim = 128
-
-    q = torch.randn(bs, q_len, num_heads_q, head_dim, dtype=torch.bfloat16, device="cuda")
-    k = torch.randn(bs, kv_len, num_heads_kv, head_dim, dtype=torch.bfloat16, device="cuda")
-    v = torch.randn(bs, kv_len, num_heads_kv, head_dim, dtype=torch.bfloat16, device="cuda")
-    out = triton_attn(q, k, v, causal=causal)
-
-    out_ref = F.scaled_dot_product_attention(
+# should the reference be computed in FP32?
+def ref_attn(q: Tensor, k: Tensor, v: Tensor, *, causal: bool = False):
+    q_len = q.shape[1]
+    kv_len = k.shape[1]
+    out = F.scaled_dot_product_attention(
         q.transpose(1, 2),
         k.transpose(1, 2),
         v.transpose(1, 2),
@@ -27,4 +19,63 @@ def test_triton_attn(q_len: int, kv_len: int, causal: bool):
         enable_gqa=True,
     ).transpose(1, 2)
 
+    return out
+
+
+@pytest.mark.parametrize("q_len,kv_len", [(64, 256), (128, 32), (33, 123), (257, 39)])
+@pytest.mark.parametrize("causal", [False, True])
+def test_triton_attn(q_len: int, kv_len: int, causal: bool):
+    bs = 2
+    q_heads = 8
+    kv_heads = 4
+    head_dim = 128
+
+    q = torch.randn(bs, q_len, q_heads, head_dim, dtype=torch.bfloat16, device="cuda")
+    k = torch.randn(bs, kv_len, kv_heads, head_dim, dtype=torch.bfloat16, device="cuda")
+    v = torch.randn(bs, kv_len, kv_heads, head_dim, dtype=torch.bfloat16, device="cuda")
+
+    out = triton_attn(q, k, v, causal=causal)
+    assert not out.isnan().any()
+
+    out_ref = ref_attn(q, k, v, causal=causal)
     torch.testing.assert_close(out, out_ref, rtol=1.6e-2, atol=1e-3)
+
+
+@pytest.mark.parametrize("causal", [False, True])
+def test_triton_varlen_attn(causal: bool):
+    bs = 4
+    q_heads = 8
+    kv_heads = 4
+    head_dim = 128
+
+    q_list = []
+    k_list = []
+    v_list = []
+    q_offsets_list = [0]
+    kv_offsets_list = [0]
+    max_q_len = 0
+
+    for _ in range(bs):
+        q_len = torch.randint(16, 256, (1,)).item()
+        kv_len = torch.randint(16, 256, (1,)).item()
+        q_list.append(torch.randn(q_len, q_heads, head_dim, dtype=torch.bfloat16, device="cuda"))
+        k_list.append(torch.randn(kv_len, kv_heads, head_dim, dtype=torch.bfloat16, device="cuda"))
+        v_list.append(torch.randn(kv_len, kv_heads, head_dim, dtype=torch.bfloat16, device="cuda"))
+        q_offsets_list.append(q_offsets_list[-1] + q_len)
+        kv_offsets_list.append(kv_offsets_list[-1] + kv_len)
+        max_q_len = max(max_q_len, q_len)
+
+    q = torch.cat(q_list, dim=0)
+    k = torch.cat(k_list, dim=0)
+    v = torch.cat(v_list, dim=0)
+    q_offsets = torch.tensor(q_offsets_list, device="cuda")
+    kv_offsets = torch.tensor(kv_offsets_list, device="cuda")
+
+    out = triton_varlen_attn(q, k, v, q_offsets, kv_offsets, max_q_len, causal=causal)
+
+    out_ref_list = []
+    for q_, k_, v_ in zip(q_list, k_list, v_list):
+        out_ref_list.append(ref_attn(q_[None], k_[None], v_[None], causal=causal)[0])
+    out_ref = torch.cat(out_ref_list, dim=0)
+
+    torch.testing.assert_close(out, out_ref)
